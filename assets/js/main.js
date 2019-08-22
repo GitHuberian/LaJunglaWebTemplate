c53cb86f792daa0ffa4febdff6656f8e953ce286
Nancy <ng.toledo@hotmail.com>
2019-08-21 19:25:03 -0500

    Home
    base template

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function openMenu() {
+  document.getElementById("menu_frame").classList.toggle("fadeIn");
+  document.getElementById("close_btn").style.display = "block";
+}
+
+function closeMenu() {
+  document.getElementById("menu_frame").classList.toggle("fadeIn");
+  document.getElementById("close_btn").style.display = "none";
+}
